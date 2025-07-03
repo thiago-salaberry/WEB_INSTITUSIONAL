a727c33da81241de87c1113d2e96f17c9a8005b3
thiago-salaberry <thiagosalaberry08@gmail.com>
2025-07-03 19:09:36 -0300

    reinicio total de la web, y aplicacion de lo nuevos diseños

--- scripts/navbar.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.fa-bars').addEventListener('click', function() {
-        const menu = document.querySelector(this.dataset.target);
-        const body = document.querySelector('body')
-        menu.classList.toggle('active');
-        body.classList.toggle('no-scroll');
-    });
