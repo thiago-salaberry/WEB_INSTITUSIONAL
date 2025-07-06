0940d368e6076f5cf189254f6f47f05bc564cce3
Campa002 <luchocampanelli1@gmail.com>
2025-07-06 20:43:08 -0300

    Responsive de inscripciones (espero que no me flate nada)

--- scripts/inscripciones.js
+++ scripts/inscripciones.js
@@ -18,9 +18,8 @@ function show_list(list_index){
     hide_every_subject();
     grade_list[list_index].classList.remove("d-none");
     grade_list[list_index].classList.add("d-flex");
-    console.log("HOLAAA")
+    console.log("Mostrando lista:", list_index);
 }
-
 
 function hide_every_container(){
     for(let i = 0; i < data_containers.length; i++){
@@ -33,6 +32,23 @@ function show_container(container_index){
     hide_every_container();
     data_containers[container_index].classList.remove("d-none");
     data_containers[container_index].classList.add("d-flex");
+    
+    // Remover clase active de todos los botones
+    document.querySelectorAll('.option-btn').forEach(btn => btn.classList.remove('active'));
+    
+    // Agregar clase active al botón correspondiente
+    const buttons = [why_button, things_button, subjects_button, when_button];
+    buttons[container_index].classList.add('active');
+}
+
+function show_subjects_grade(grade_index){
+    show_list(grade_index);
+    
+    // Remover clase active de todos los botones de materias
+    document.querySelectorAll('.button_subjects').forEach(btn => btn.classList.remove('active'));
+    
+    // Agregar clase active al botón correspondiente
+    button_subjects[grade_index].classList.add('active');
 }
 
 why_button.onclick = function(){show_container(0);};
@@ -40,6 +56,6 @@ things_button.onclick = function(){show_container(1);};
 subjects_button.onclick = function(){show_container(2);};
 when_button.onclick = function(){show_container(3);};
 
-button_subjects[0].onclick = function(){show_list(0);};
-button_subjects[1].onclick = function(){show_list(1);};
-button_subjects[2].onclick = function(){show_list(2);};
+button_subjects[0].onclick = function(){show_subjects_grade(0);};
+button_subjects[1].onclick = function(){show_subjects_grade(1);};
+button_subjects[2].onclick = function(){show_subjects_grade(2);};
